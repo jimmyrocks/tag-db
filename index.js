b784c86dfdd140df527d6dab9cf9f4a8f6c7441d
Jim McAndrew <jim@loc8.us>
2014-05-28 10:59:04 -0600

    Don't create new table if we don't need to

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var writeTable = require('./src/writeTable');
 
-writeTable.createTable(function(ce, cr) {
+//writeTable.createTable(function(ce, cr) {
   writeTable.updateTable(function(e, r) {
-    console.log(e);
-    console.log(JSON.stringify(r, null,2));
+    process.exit(0);
   });
-});
+//});
