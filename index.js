fab67818416e9d13f9d51af10e6bd06779a90c0d
Jim McAndrew <jim@loc8.us>
2014-12-31 14:22:53 -0700

    Add error reporting and TODOs

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var writeTable = require('./src/writeTable');
+var datagres = require('datagres')(require('./config'));
+var writeTable = require('./src/writeTable')(datagres);
 
-//writeTable.createTable(function(ce, cr) {
-  writeTable.updateTable(function(e, r) {
-    process.exit(0);
-  });
-//});
+// TODO: Take a parameter to either write to the DB or to read from the DB
+// TODO: Create code to read from the DB into files
+// TODO: Report Progress
+
+writeTable.createTable(function(e) {
+  if (e) {
+    throw (e.stack ? e : new Error(e));
+  } else {
+    writeTable.updateTable(function(writeE) {
+      if (writeE) throw (writeE.stack ? writeE : new Error(writeE));
+      process.exit(0);
+    });
+  }
+});
